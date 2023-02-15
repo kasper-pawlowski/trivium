715ab06fbc49eb089ab4f81fa35ae400dc2ddba5
kasper-pawlowski <kapi28000@gmail.com>
2023-02-15 22:13:02 +0100

    + - FullyQualifiedErrorId:CannotConvertArgumentNoMessage,Microsoft.PowerShell.Commands.GetDateCommand

--- pushtogit.bat
+++ pushtogit.bat
@@ -1,9 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
 
-for /f "usebackq tokens=1-6 delims=/: " %%d in (`powershell Get-Date -Format dd-MM-yyyy-HH-mm`) do (
-    set "current_date=%%d-%%e-%%f"
-    set "current_time=%%g:%%h"
+for /f "usebackq tokens=1-6 delims=/: " %%d in (`powershell Get-Date -Format "dd.MM.yyyy - HH:mm"`) do (
+    set "current_date=%%d"
+    set "current_time=%%e:%%f"
 )
 
 set "commit_msg=%current_date% - %current_time%"
