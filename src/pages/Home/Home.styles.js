850880f84e3f2d738d429306eaa0e31bdfa6fcff
kasper-pawlowski <kapi28000@gmail.com>
2023-02-16 17:36:16 +0100

    Automatic commit on 16.02.2023 at 17-36-15,97

--- src/pages/Home/Home.styles.js
+++ src/pages/Home/Home.styles.js
@@ -5,5 +5,8 @@ export const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding: 40px;
+    padding: 10px;
+    @media screen and (min-width: 768px) {
+        padding: 40px;
+    }
 `;
