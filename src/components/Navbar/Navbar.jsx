10e8782608cc96ef4994d7e0d830001b80abb56d
kasper-pawlowski <kapi28000@gmail.com>
2023-02-17 19:57:18 +0100

    Automatic commit on 17.02.2023 at 19-57-18,62

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { Avatar, Caret, StyledLink, UserContainer, Wrapper } from './Navbar.styles';
 import TriviumLogo from '@assets/triviumLogo.svg';
 import { useUserAuth } from '@contexts/AuthContext';
+import { useMediaQuery } from 'react-responsive';
 
 const Navbar = () => {
+    const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+
     const {
         user: { photoURL },
     } = useUserAuth();
@@ -13,10 +16,12 @@ const Navbar = () => {
             <StyledLink to="/">
                 <img src={TriviumLogo} alt="" draggable={false} />
             </StyledLink>
-            <UserContainer>
-                <Avatar src={photoURL} alt="" draggable={false} />
-                <Caret />
-            </UserContainer>
+            {!isMobile && (
+                <UserContainer>
+                    <Avatar src={photoURL} alt="" draggable={false} />
+                    <Caret />
+                </UserContainer>
+            )}
         </Wrapper>
     );
 };
