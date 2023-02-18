b1a44dacfe7029fb0c2dad46307b384b062adc3c
kasper-pawlowski <kapi28000@gmail.com>
2023-02-18 12:19:03 +0100

    Automatic commit on 18.02.2023 at 12-19-03,39

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
 
     return (
         <Wrapper>
-            <Logo src={logo} alt="" />
+            <Logo src={logo} alt="" draggable={false} />
             <H1>Trivium</H1>
             <H2>Expand your knowledge and play alone or with friends to learn something new every time with Trivium's 4,098 verified questions.</H2>
             <Search setSearchValue={setSearchValue} />
