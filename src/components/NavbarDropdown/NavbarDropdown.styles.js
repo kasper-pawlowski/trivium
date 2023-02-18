f2566b46ef40a3b1e0427fce7e261137e668fd98
kasper-pawlowski <kapi28000@gmail.com>
2023-02-18 23:55:31 +0100

    Automatic commit on 18.02.2023 at 23-55-31,13

--- src/components/NavbarDropdown/NavbarDropdown.styles.js
+++ src/components/NavbarDropdown/NavbarDropdown.styles.js
@@ -7,6 +7,7 @@ export const Wrapper = styled.div`
     align-items: center;
     position: relative;
     user-select: none;
+    z-index: 999;
 
     .drop {
         display: flex;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,20 +2,43 @@ import React, { useState } from 'react';
 import { Logo, StyledFcGoogle, Wrapper } from './Login.styles';
 import triviumLogoWithText from '@assets/triviumLogoWithText.svg';
 import StyledButton from '@components/StyledButton';
-import { auth, provider } from '@services/firebase';
+import { auth, db, provider } from '@services/firebase';
 import { signInWithPopup } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
+import { collection, doc, getDocs, query, setDoc, where } from 'firebase/firestore';
 
 const Login = () => {
-    const navigate = useNavigate();
-    const [error, setError] = useState(null);
+    const signInWithGoogle = async () => {
+        try {
+            const res = await signInWithPopup(auth, provider);
+            const user = res.user;
+            const q = query(collection(db, 'users'), where('googleUid', '==', user.uid));
+            const docs = await getDocs(q);
+            if (docs.docs.length === 0) {
+                let randomUid = Math.floor(1000 + Math.random() * 9000).toString();
 
-    const signInWithGoogle = () => {
-        signInWithPopup(auth, provider).catch((error) => {
-            const errorMessage = error.message;
-            setError('Nie udało się zalogować');
-            console.log(errorMessage);
-        });
+                // Check if uid exists in Firestore collection
+                let randomUidQuery = query(collection(db, 'users'), where('uid', '==', randomUid));
+                let querySnapshot = await getDocs(randomUidQuery);
+
+                if (querySnapshot.size > 0) {
+                    // If uid already exists, generate a new one and check again
+                    while (querySnapshot.size > 0) {
+                        randomUid = Math.floor(1000 + Math.random() * 9000).toString();
+                        randomUidQuery = query(collection(db, 'users'), where('uid', '==', randomUid));
+                        querySnapshot = await getDocs(randomUidQuery);
+                    }
+                }
+                await setDoc(doc(db, 'users', user.uid), {
+                    displayName: user.displayName,
+                    photoURL: user.photoURL,
+                    googleUid: user.uid,
+                    uid: randomUid,
+                });
+            }
+        } catch (err) {
+            console.error(err);
+            alert(err.message);
+        }
     };
 
     return (
@@ -24,7 +47,6 @@ const Login = () => {
             <StyledButton onClick={signInWithGoogle}>
                 <StyledFcGoogle /> Login with Google
             </StyledButton>
-            {error && <p>{error}</p>}
         </Wrapper>
     );
 };
